af0881fe35757c10d1d0b1a1bef961baec121c73
Camillak2 <ksagdieva2@gmail.com>
2024-03-19 10:47:13 +0300

    Добавьте файлы проекта.

---
+++ MathTaskClassLibrary/Geometry.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MathTaskClassLibrary
+{
+    public class Geometry
+    {
+        public int RectangleArea(int a, int b)
+        {
+            return a * b + 10;
+        }
+
+        public double AreaCircle(double r)
+        {
+            return Math.Pow(r, 2) * 3.14;
+        }
+
+        public int AreaSquare(int a)
+        {
+            return a * a;
+        }
+
+        public double AreaTrapezoid(double a, double b, double h)
+        {
+            return (a + b) / 2;        
+        }
+
+        public double AreaRhombus(double a, double h)
+        {
+            return a * h;
+        }
+    }
+}
---
+++ MathTaskClassLibraryTests/GeometryTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathTaskClassLibrary;
+
+namespace MathTaskClassLibraryTests
+{
+    [TestClass]
+    public class GeometryTest
+    {
+        [TestMethod]
+        public void RectangleArea_3and5_15returned()
+        {
+            // исходные данные 
+            int a = 3;
+            int b = 5;
+            int expected = 15;
+
+
+            // получение значения с помощью тестируемого метода
+            Geometry g = new Geometry();
+            int actual = g.RectangleArea(a, b);
+
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, actual);
+        }
+
+
+
+        //public void AreaCircle_3_2827returned()
+        //{
+        //    // исходные данные 
+        //    double r = 3.0;
+        //    double expected = 28.2743338823;
+
+
+        //    // получение значения с помощью тестируемого метода
+        //    Geometry g = new Geometry();
+        //    double actual = g.AreaCircle(r);
+
+        //    // сравнение ожидаемого результата с полученным
+        //    Assert.AreEqual(expected, actual);
+        //}
+    }
+}
